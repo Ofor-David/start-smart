8c11a156ab3158a9de83bff3be6b4f3f012c87fe
Ofor David <davidoforincloud@gmail.com>
2025-07-24 08:58:59 +0100

    added: response handler

--- terraform/lambda.py
+++ terraform/lambda.py
@@ -2,33 +2,28 @@
 
 import json
 
+
+def make_response(status_code, body_dict):
+    # Wraps the response in proper API Gateway format with CORS header
+    return {
+        "statusCode": status_code,
+        "body": json.dumps(body_dict),
+        # udpdate later for security
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",  # <--- Allow CORS
+            "Access-Control-Allow-Headers": "*",  # <--- Allow headers
+            "Access-Control-Allow-Methods": "OPTIONS,POST",
+        },
+    }
+
+
 def handler(event, context):
-    print("Received event:", json.dumps(event))
+    print(
+        "Received event:", json.dumps(event)
+    )  # turns event dictionary into string For cloudwatch logs
 
     # add enrichment/validation here
-    body = json.loads(event['body'])
+    body = json.loads(event["body"])  # parses req payload to python dict
 
-    """ return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Event received",
-            "data": body
-        }),
-        "headers": {
-            "Content-Type": "application/json"
-        }
-    } """
-    return {
-    "statusCode": 200,
-    "body": json.dumps({
-        "message": "Event received",
-        "data": body
-    }),
-    "headers": {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",   # <--- Allow CORS
-        "Access-Control-Allow-Headers": "*",  # <--- Allow headers
-        "Access-Control-Allow-Methods": "OPTIONS,POST"
-    }
-}
-
+    make_response(200, {"message": "Event recerived", "data": body})
