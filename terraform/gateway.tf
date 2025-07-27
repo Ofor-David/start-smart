9768bfd026f0ca2513f11f27a2a5dcc1f890a7f9
Ofor David <davidoforincloud@gmail.com>
2025-07-27 21:08:22 +0100

    fix: error with apigateway deployment

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -71,6 +71,7 @@ resource "aws_api_gateway_method_response" "options_response" {
 }
 
 resource "aws_api_gateway_integration_response" "options_integration_response" {
+  depends_on = [ aws_api_gateway_integration.options_integration ]
   rest_api_id = aws_api_gateway_rest_api.startsmart_api.id
   resource_id = aws_api_gateway_resource.track.id
   http_method = aws_api_gateway_method.options_track.http_method
